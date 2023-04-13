9e8abd1d9a5d296d3600ad66e77c0854dc0f2fb6
wvvk <winwkam@gmail.com>
2023-04-12 17:42:50 -0700

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,16 +1,9 @@
 import random
 
-while 1:
-    n_char = int(input("how many chars:"))
-    n_sym = int(input("how many symbols:"))
-    n_num = int(input("how many num:"))
-
+def generate_password(n_char, n_sym, n_num):
     num_list = [str(i) for i in range(0, 10)]
     char_list = [chr(c) for c in range(65, 91)] + [chr(c) for c in range(97, 123)]
     sym_list = [chr(c) for c in range(35, 44)]
-    # print(num_list)
-    # print(char_list)
-    # print(sym_list)
 
     # 1 pick the random num for each type
     selectedCharList = [random.choice(char_list) for i in range(n_char)]
@@ -27,3 +20,13 @@ while 1:
     while selectedList:
         password += selectedList.pop(random.randint(0, len(selectedList) - 1))
     print(password)
+    return password
+
+while 1:
+    n_char = int(input("how many chars:"))
+    n_sym = int(input("how many symbols:"))
+    n_num = int(input("how many num:"))
+
+    password = generate_password(n_char, n_sym, n_num)
+
+    print(f"Your password is: {password}")
